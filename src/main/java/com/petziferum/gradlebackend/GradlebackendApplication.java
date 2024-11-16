b70238a69d9a89d0e4d4164e7e2be091fb3efcdb
petziferum <daboarderpjb@gmail.com>
2024-11-16 12:22:25 +0100

    redirect controller

--- src/main/java/com/petziferum/gradlebackend/GradlebackendApplication.java
+++ src/main/java/com/petziferum/gradlebackend/GradlebackendApplication.java
@@ -9,5 +9,4 @@ public class GradlebackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GradlebackendApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/petziferum/gradlebackend/configuration/RedirectController.java
@@ -0,0 +1,13 @@
+package com.petziferum.gradlebackend.configuration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui/index.html#/";
+    }
+}
